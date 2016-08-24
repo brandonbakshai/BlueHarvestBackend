2c01ffbffd751a54caf978d787a5121f1236640b
brandonbakhshai <brandon.bakhshai@columbia.edu>
2016-08-24 19:39:24 -0400

    added some doc comments

--- src/routes/utility.js
+++ src/routes/utility.js
@@ -1,17 +1,38 @@
 'use strict';
 
+/**
+ *
+ * @param model
+ * @param req
+ * @param res
+ * @returns {Promise.<TResult>|Promise}
+ */
 function createHelper(model, req, res) {
   return model.createItem(req.body)
   .then(item => res.send(item))
   .catch(err => res.send(err));
 }
 
+/**
+ *
+ * @param model
+ * @param req
+ * @param res
+ * @returns {Promise.<T>|Promise}
+ */
 function getHelper(model, req, res) {
   return model.getItems({ _id: req.params.bounty_id })
   .then(item => res.send(item))
   .catch(err => res.send(err));
 }
 
+/**
+ *
+ * @param model
+ * @param req
+ * @param res
+ * @returns {Promise.<T>|Promise}
+ */
 function getAllHelper(model, req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -20,36 +41,77 @@ function getAllHelper(model, req, res) {
   .catch(err => res.send(err));
 }
 
+/**
+ *
+ * @param model
+ * @param req
+ * @param res
+ * @returns {Promise.<TResult>|Promise}
+ */
 function deleteHelper(model, req, res) {
   return model.deleteItem(req.params.bounty_id)
   .then(() => res.send('success'))
   .catch(err => res.send(err));
 }
 
+/**
+ *
+ * @param model
+ * @param req
+ * @param res
+ * @returns {Promise.<TResult>|Promise}
+ */
 function updateDataHelper(model, req, res) {
   return model.updateData(req.params.bounty_id, req.body)
   .then(bounty => res.send(bounty))
   .catch(err => res.send(err));
 }
 
+/**
+ *
+ * @param model
+ * @param req
+ * @param res
+ * @returns {Promise.<TResult>|Promise}
+ */
 function upvoteHelper(model, req, res) {
   return model.upvote(req.params.bounty_id)
   .then(bounty => res.send(bounty))
   .catch(err => res.send(err));
 }
 
+/**
+ *
+ * @param model
+ * @param req
+ * @param res
+ * @returns {Promise.<TResult>|Promise}
+ */
 function downvoteHelper(model, req, res) {
   return model.downvote(req.params.bounty_id)
   .then(bounty => res.send(bounty))
   .catch(err => res.send(err));
 }
 
+/**
+ *
+ * @param model
+ * @param req
+ * @param res
+ * @returns {Promise.<TResult>|Promise}
+ */
 function incrementViewsHelper(model, req, res) {
   return model.incrementViews(req.params.bounty_id)
   .then(bounty => res.send(bounty))
   .catch(err => res.send(err));
 }
 
+/**
+ *
+ * @param err
+ * @param res
+ * @returns {*}
+ */
 function errorHandler(err, res) {
   console.log(`error: ${err}`);
   return res.send(`error: ${err}`);
